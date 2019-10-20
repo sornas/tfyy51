fac231875693560adcbd6e3803592e27674dd2b4
Gustav Sörnäs <gusso230@student.liu.se>
2019-10-20 13:57:56 +0200

    Fix put_text
    - Prints in "formatting mode" instead of terminal mode
    - Correct argument formatting

--- Kod/bilbana/yc4/display/put_text.m
+++ Kod/bilbana/yc4/display/put_text.m
@@ -1,9 +1,9 @@
 function [pkg] = put_text(x, y, justification, text)
-	code = double(join('Z', justification))
+	code = double(strjoin({'Z', justification}, ''));
 
-	arg1 = [x, 0, y, 0]
+	arg1 = [x, 0, y, 0];
 	arg2 = text;
 	arg3 = 0;
 
-	pkg = get_package(join('Z', justification), [arg1 arg2 arg3])
+	pkg = get_package(code, [arg1 double(arg2) arg3]);
 end
