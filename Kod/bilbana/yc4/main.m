4fd91c8fd7e72e248831b4c55742d1dc96dc2be1
Gustav Sörnäs <gusso230@student.liu.se>
2019-10-22 17:30:19 +0200

    Implement #21

--- Kod/bilbana/yc4/main.m
+++ Kod/bilbana/yc4/main.m
@@ -22,18 +22,24 @@ start_race(1)
 start_race(2)
 
 car1 = struct;
+car1.running = false;
 car1.segment = 1;
 car1.lap = 0;
 car1.lap_times = [];
 car1.seg_times = [];
 
 car2 = struct;
+car2.running = false;
 car2.segment = 1;
 car2.lap = 0;
 car2.lap_times = [];
 car2.seg_times = [];
 
 tocs = [];
+
+%% ASK ACTIVE CARS
+car1.running = (prompt('Vill du köra bil 1? (J/N) [N]', 's') == 'J')
+car2.running = (prompt('Vill du köra bil 2? (J/N) [N]', 's') == 'J')
 
 %% MAIN LOOP
 while 1
@@ -48,91 +54,106 @@ while 1
     drawnow
     
     %% READ
-    [car1.new_lap, car1.new_check_point, car1.time] = get_car_position(1);
-    [car2.new_lap, car2.new_check_point, car2.time] = get_car_position(2);
+    if car1.running == true
+		[car1.new_lap, car1.new_check_point, car1.time] = get_car_position(1);
+	end
+	if car2.running == true
+		[car2.new_lap, car2.new_check_point, car2.time] = get_car_position(2);
+	end
     
     %% CHECK LAP AND CHECKPOINT (CAR 1)
-    if car1.new_check_point == true
-        % beep;
-        if car1.lap ~= 0
-            car1.seg_times(car1.lap, car1.segment) = toc(car1.seg_tic);
-        end
-        car1.segment = car1.segment + 1;
-        car1.seg_tic = tic;
-    end
-    if car1.new_lap == true
-        if car1.lap == 0
-            % dont save time for first lap
-            car1.segment = 1;
-            car1.lap = car1.lap + 1;
-            car1.seg_tic = tic;
-            car1.lap_tic = tic;
-            continue;
-        end
-        beep;
-        car1.seg_times(car1.lap, car1.segment) = toc(car1.seg_tic);
-        car1.seg_tic = tic;
-        car1.lap_times(car1.lap) = toc(car1.lap_tic);
-        car1.lap_tic = tic;
+	if car1.running == true
+			if car1.new_check_point == true
+				% beep;
+				if car1.lap ~= 0
+					car1.seg_times(car1.lap, car1.segment) = toc(car1.seg_tic);
+				end
+				car1.segment = car1.segment + 1;
+				car1.seg_tic = tic;
+			end
+			if car1.new_lap == true
+				if car1.lap == 0
+					% dont save time for first lap
+					car1.segment = 1;
+					car1.lap = car1.lap + 1;
+					car1.seg_tic = tic;
+					car1.lap_tic = tic;
+					continue;
+				end
+				beep;
+				car1.seg_times(car1.lap, car1.segment) = toc(car1.seg_tic);
+				car1.seg_tic = tic;
+				car1.lap_times(car1.lap) = toc(car1.lap_tic);
+				car1.lap_tic = tic;
 
-        display_data = {display_data, put_text(100, 32, 'L', strjoin({num2str(car1.lap), get_time_as_string(round(car1.lap_times(car1.lap) * 1000))}, ' '))};
-        
-        car1.segment = 1;
-        car1.lap = car1.lap + 1;
-        
-        car1.seg_times(car1.lap, 1) = 0;
-    end
-    
+				display_data = {display_data, put_text(100, 32, 'L', strjoin({num2str(car1.lap), get_time_as_string(round(car1.lap_times(car1.lap) * 1000))}, ' '))};
+				
+				car1.segment = 1;
+				car1.lap = car1.lap + 1;
+				
+				car1.seg_times(car1.lap, 1) = 0;
+			end
+	end
+
     %% CHECK LAP AND CHECKPOINT (CAR 2)
-    if car2.new_check_point == true
-        % beep;
-        if car2.lap ~= 0
-            car2.seg_times(car2.lap, car2.segment) = toc(car2.seg_tic);
-        end
-        car2.segment = car2.segment + 1;
-        car2.seg_tic = tic;
-    end
-    if car2.new_lap == true
-        if car2.lap == 0
-            % dont save time for first lap
-            car2.segment = 1;
-            car2.lap = car2.lap + 1;
-            car2.seg_tic = tic;
-            car2.lap_tic = tic;
-            disp('continuing');
-            continue;
-        end
-        beep;
-        
-        car2.seg_times(car2.lap, car2.segment) = toc(car2.seg_tic);
-        car2.seg_tic = tic;
-        car2.lap_times(car2.lap) = toc(car2.lap_tic);
-        car2.lap_tic = tic;
+	if car2.running == true
+		if car2.new_check_point == true
+			% beep;
+			if car2.lap ~= 0
+				car2.seg_times(car2.lap, car2.segment) = toc(car2.seg_tic);
+			end
+			car2.segment = car2.segment + 1;
+			car2.seg_tic = tic;
+		end
+		if car2.new_lap == true
+			if car2.lap == 0
+				% dont save time for first lap
+				car2.segment = 1;
+				car2.lap = car2.lap + 1;
+				car2.seg_tic = tic;
+				car2.lap_tic = tic;
+				disp('continuing');
+				continue;
+			end
+			beep;
+			
+			car2.seg_times(car2.lap, car2.segment) = toc(car2.seg_tic);
+			car2.seg_tic = tic;
+			car2.lap_times(car2.lap) = toc(car2.lap_tic);
+			car2.lap_tic = tic;
 
-        display_data = {display_data, put_text(100, 48, 'L', strjoin({num2str(car2.lap), get_time_as_string(round(car2.lap_times(car2.lap) * 1000))}, ' '))};
-        
-        car2.segment = 1;
-        car2.lap = car2.lap + 1;
-        
-        car2.seg_times(car2.lap, 1) = 0;
-    end
-    
-    %% CALCULATE (CAR 1)
-    car1.car_constant = get_car_constant(1);
-    car1.v = get_new_v(car1.segment);
-    car1.track_u_constant = get_track_u_constant();
-    car1.u = get_new_u(car1.v, car1.car_constant, car1.track_u_constant);
+			display_data = {display_data, put_text(100, 48, 'L', strjoin({num2str(car2.lap), get_time_as_string(round(car2.lap_times(car2.lap) * 1000))}, ' '))};
+				
+			car2.segment = 1;
+			car2.lap = car2.lap + 1;
+			
+			car2.seg_times(car2.lap, 1) = 0;
+		end
+	end
+
+	%% CALCULATE (CAR 2)
+	if car1.running == true
+		car1.car_constant = get_car_constant(1);
+		car1.v = get_new_v(car1.segment);
+		car1.track_u_constant = get_track_u_constant();
+		car1.u = get_new_u(car1.v, car1.car_constant, car1.track_u_constant);
+	end
     
     %% CALCULATE (CAR 2)
-    car2.car_constant = get_car_constant(2);
-    car2.v = get_new_v(car2.segment);
-    car2.track_u_constant = get_track_u_constant();
-    car2.u = get_new_u(car2.v, car2.car_constant, car2.track_u_constant);
+	if car2.running == true
+		car2.car_constant = get_car_constant(2);
+		car2.v = get_new_v(car2.segment);
+		car2.track_u_constant = get_track_u_constant();
+		car2.u = get_new_u(car2.v, car2.car_constant, car2.track_u_constant);
+	end
     
     %% EXECUTE
-    set_car_speed(1, car1.u);
-    set_car_speed(2, car2.u);
-    
+    if car1.running == true
+		set_car_speed(1, car1.u);
+	end
+	if car2.running == true
+   		set_car_speed(2, car2.u);
+	end
     %% DISPLAY
     
     %% END OF LOOP
