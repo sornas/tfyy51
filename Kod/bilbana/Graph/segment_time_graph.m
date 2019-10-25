785a87c8a5277abf09f44086b7dd05d2ebfb5af5
Albin <albwa833@student.liu.se>
2019-10-25 10:29:35 +0200

    Bugfix graph. Text visas nu ordentligt

--- Kod/bilbana/Graph/segment_time_graph.m
+++ Kod/bilbana/Graph/segment_time_graph.m
@@ -34,7 +34,7 @@ Plot = bar(avr_seg_time);
 Plot.FaceColor = 'k';
 xlabel('Segment');
 ylabel('Tid [s]');
-tit = char(['Medeltid/segment bana',char(track)]);
+tit = ['Medeltid/segment bana ' num2str(track)];
 title(tit); 
 end
 
