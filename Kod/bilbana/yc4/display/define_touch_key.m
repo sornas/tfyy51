47394320ad1e33f9a0302b6fa814ede543eba0ac
Gustav Sörnäs <gusso230@student.liu.se>
2019-10-27 14:26:09 +0100

    Second wave

---
+++ Kod/bilbana/yc4/display/define_touch_key.m
@@ -0,0 +1,7 @@
+function pkg = define_touch_key(x1, y1, x2, y2, down_code, up_code, just, text)
+	arg_location = [get_bytes(x1), get_bytes(y1), get_bytes(x2), get_bytes(y2)];
+	arg_text = double(strjoin({just; text}, ''));
+	arg_null = 0;
+
+	pkg = get_package('AT', [arg_location, down_code, up_code, arg_text, arg_null]);
+end
