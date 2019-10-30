645610a6b634f548e0d09efa1b57d6b3c70e0937
David Thorén <davth346@student.liu.se>
2019-10-30 16:29:43 +0100

    Lade till kod i main som sparar variablerna i structen i en csv (kan ändras) efter körning

--- Kod/bilbana/yc4/main.m
+++ Kod/bilbana/yc4/main.m
@@ -26,6 +26,7 @@ car1.segment = 1;
 car1.lap = 0;
 car1.lap_times = [];
 car1.seg_times = [];
+car1Table = struct2table(car1);
 
 car2 = struct;
 car2.segment = 1;
@@ -151,3 +152,11 @@ matlabclient(3);
 
 graphs(car1.lap_times, 13, car1.seg_times, 1);
 graphs(car2.lap_times, 13, car2.seg_times, 2);
+
+%% SAVE VARIABLES FROM CAR STRUCT
+
+dateStr = datestr(now, 'dd-mmm-yyyy');
+timeStr = datestr(now, 'HH.MM');
+strCsv = [dateStr,'_',timeStr, '.csv'];
+filename = join(strCsv);
+writetable(car1Table, filename);
