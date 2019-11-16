6f56856ecdcea0f873a154369b4f544cef12f470
Gustav Sörnäs <gusso230@student.liu.se>
2019-11-16 14:04:21 +0100

    Fix put_text

--- Kod/bilbana/yc4/display/text.m
+++ Kod/bilbana/yc4/display/text.m
@@ -1,6 +1,6 @@
-function [pkg] = text(x, y, justification, text)
-arg1 = [mod(x, 256), x ./ 256, mod(y, 256), y ./ 256];
-arg2 = strrep(text, '\n', '|');
+function [pkg] = put_text(x, y, justification, text)
+arg1 = [get_bytes(x), get_bytes(y)];
+arg2 = text;
 arg3 = 0;
 
 pkg = get_package(strjoin({'Z', justification}, ''),  [arg1 double(arg2) arg3]);
