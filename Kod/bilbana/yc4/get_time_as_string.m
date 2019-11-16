ce027a482fa3fa18f0409cc8fde3bc1a9a67c6f7
Gustav Sörnäs <gusso230@student.liu.se>
2019-11-16 14:17:57 +0100

    Fix get_time_as_string (again ...)

--- Kod/bilbana/yc4/get_time_as_string.m
+++ Kod/bilbana/yc4/get_time_as_string.m
@@ -1,10 +1,16 @@
 function num_as_str = get_time_as_string(millis)
 %GET_TIME_AS_STRING Number of milliseconds, formatted mm:ss.s and rounded
 %   Detailed explanation goes here
-minutes = num2str(fix(millis / (1000*60)));
+minutes = fix(millis / (1000*60));
 seconds = mod(millis, 1000*60);
+rest = round(mod(seconds, 1000), -2) / 100;
+if rest == 10
+    seconds = seconds + 1*1000;
+    rest = 0;
+end
+minutes_str = num2str(minutes);
 seconds_str = sprintf('%02d', fix(seconds / 1000));
-rest_str = num2str(round(mod(seconds, 1000), -2) / 100);
-num_as_str = strjoin({minutes, ':', seconds_str, '.', rest_str}, '');
+rest_str = num2str(rest);
+num_as_str = strjoin({minutes_str, ':', seconds_str, '.', rest_str}, '');
 end
 
