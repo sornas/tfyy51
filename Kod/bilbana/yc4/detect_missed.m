0e1a9de685c999fdd0e5252c741cef9d06b16451
Gustav Sörnäs <gusso230@student.liu.se>
2019-11-19 16:22:33 +0100

    NOT WORKING seg_len and track_len

--- Kod/bilbana/yc4/detect_missed.m
+++ Kod/bilbana/yc4/detect_missed.m
@@ -1,9 +1,7 @@
 function [out] = detect_missed( position, segment, track)
 %DETECT_MISSED Retunerar true om position ligger utanför nuvarande segment
 %   
-track_len = [2.53 3.05 4.73 7.68 8.98 10.93 14.96 17.57 19.60;
-             2.53 3.05 4.92 7.60 8.84 10.65 14.68 17.76 19.95];
-a = track_len(track, segment);
+a = track_len(segment);
 out = a <= position;
 end
 
--- Kod/bilbana/yc4/get_seg_constant.m
+++ Kod/bilbana/yc4/get_seg_constant.m
@@ -1,4 +1,4 @@
-function [out] = get_seg_constant(position, lap_constants, track)
+function [out] = get_seg_constant(position, lap_constants, track, track_len)
 %GET_SEG_CONSTANT Avgör vilken seg_constat som ska användas utifån
 %nuvarande position.
 %{   
@@ -6,10 +6,8 @@ position - Position i meter från start
 lap_constants - De seg_constants som ska användas detta varv
 track - den bana/bil som beräkningarna ska göras för
 %}
-track_len = [0 2.53 3.05 4.73 7.68 8.98 10.93 14.96 17.57;
-             0 2.53 3.05 4.92 7.60 8.84 10.65 14.68 17.76];
 for i = 1:length(track_len)
-    if position >= track_len(track, i)
+    if position >= track_len(i)
         seg_constant_num = i;
     end
 end
