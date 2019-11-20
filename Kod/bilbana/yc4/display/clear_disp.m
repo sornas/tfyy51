beb0792f35d52cb1f95ecb7c3c4342740b6bc53e
Gustav Sörnäs <gusso230@student.liu.se>
2019-11-20 15:10:56 +0100

    Rename conflicting function-names

--- Kod/bilbana/yc4/display/clear_disp.m
+++ Kod/bilbana/yc4/display/clear_disp.m
@@ -1,3 +1,3 @@
-function pkg = clear()
+function pkg = clear_disp()
 pkg = get_package('DL', []);
 end
--- Kod/bilbana/yc4/display/draw_line.m
+++ Kod/bilbana/yc4/display/draw_line.m
@@ -1,4 +1,4 @@
-function pkg = line(x1, y1, x2, y2)
+function pkg = draw_line(x1, y1, x2, y2)
 arg = [mod(x1, 255), fix(x1 / 255), y1, 0, mod(x2, 255), fix(x2 / 255), y2, 0];
 pkg = get_package('GD', arg);
 end
