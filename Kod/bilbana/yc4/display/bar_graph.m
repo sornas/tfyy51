d963e174c1ab177a934797c87dcb466b2f9ecd19
Gustav Sörnäs <gusso230@student.liu.se>
2019-11-20 18:12:09 +0100

    Changes

--- Kod/bilbana/yc4/display/bar_graph.m
+++ Kod/bilbana/yc4/display/bar_graph.m
@@ -1,7 +1,6 @@
 function pkg = bar_graph(direction, no, x1, y1, x2, y2, start_value, end_value, type, pattern)
-pkg = []
-args = [no, get_bytes(x1), get_bytes(y1), get_bytes(x2), get_bytes(y2)]
-args = [args, start_value, end_value, type, pattern]  %TODO get_bytes or no ?
+args = [no, get_bytes(x1), get_bytes(y1), get_bytes(x2), get_bytes(y2)];
+args = [args, start_value, end_value, type, pattern];  %TODO get_bytes or no ?
 
-pkg = get_package(strjoin({'B', direction}, ''), args)
+pkg = get_package(strjoin({'B', direction}, ''), args);
 end
